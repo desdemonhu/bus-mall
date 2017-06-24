664c764ef18bfdfe68a52a8b4e6fb20c32a96af4
Raegan Millhollin <desdemonhu@gmail.com>
2017-06-24 09:30:29 -0700

    Added user stories and a property that creates an id tag based on the file name

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
-///Display 3 products at a time
+///Display 3 products at a time, new each time
+///Click on image and track number of clicks on image
 ///Select 1 of 3 then change all images?
 ///Track total number of clicks on image
 ///Precentage of times an item was clicked when it was shown (display vs clicked)
-///25 selections then results are displayed
-
+///25 selections then results are displayed. No clicks anymore
+///Results of all images # of votes for nameOfImage
 
 var imageEls = document.getElementsByClassName('medium');
 
@@ -13,6 +14,10 @@ function Image(name, path){
   this.path = path;
   this.shown = 0;
   this.clicked = 0;
+  var shortName = path.split('/');
+  shortName = shortName[1].split('.');
+  shortName = shortName[0];
+  this.idTag = 'img-' + shortName;
 }
 
 ///Create a function that creates new Image objects and push to images array
@@ -22,5 +27,5 @@ var images = [
   new Image('bag','assets/bag.jpg'),
   new Image('banana', 'assets/banana.jpg')
 ];
-
+console.log(images[0].idTag);
 //This is how to get to an images current url:   imageEls["0"].currentSrc
