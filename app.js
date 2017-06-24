b14c5bd83714424dee7e1fc4c0ec6691cdd85abf
Raegan Millhollin <desdemonhu@gmail.com>
2017-06-24 10:27:56 -0700

    Random first set of images done!

--- app.js
+++ app.js
@@ -7,7 +7,6 @@
 ///25 selections then results are displayed. No clicks anymore
 ///Results of all images # of votes for nameOfImage
 
-var imageEls = document.getElementsByClassName('medium');
 
 function Image(name, path){
   this.name = name;
@@ -20,12 +19,60 @@ function Image(name, path){
   this.idTag = 'img-' + shortName;
 }
 
+//This is how to get to an images current url:   imageEls["0"].currentSrc
+
+Image.prototype.changeImage = function(){
+  ///Get id of image clicked
+  ///Change .src of that ID
+};
 ///Create a function that creates new Image objects and push to images array
 
 ///create an object for each image
+
+function imgClick(event){
+  /// Get's image in image array and updates +1 clicked
+  ///changes image
+  ///Get's new image from array and updates shown +1
+}
+
+///Displays first set of images at random
+function randomImages() {
+  ///random number from number of items in array
+  var randomOne = Math.floor(Math.random() * ((images.length - 1) - 0 + 1)) + 0;
+  var randomTwo = Math.floor(Math.random() * ((images.length - 1) - randomOne + 1)) + 0;
+  var randomThree = Math.floor(Math.random() * ((images.length - 1) - randomTwo + 1)) + 0;
+  ///Gets path of image and puts in src of image ids
+  firstImageEl.src = images[randomOne].path;
+  secondImageEl.src = images[randomTwo].path;
+  thirdImageEl.src = images[randomThree].path;
+}
+
 var images = [
   new Image('bag','img/bag.jpg'),
-  new Image('banana', 'img/banana.jpg')
+  new Image('banana', 'img/banana.jpg'),
+  new Image('bathroom','img/bathroom.jpg'),
+  new Image('boots', 'img/boots.jpg'),
+  new Image('breakfast', 'img/breakfast.jpb'),
+  new Image('bubblegum', 'img/bubblegum'),
+  new Image('chair', 'img/chair.jpg'),
+  new Image('cthulhu', 'img/cthulhu.jpg'),
+  new Image('dog-duck', 'img/dog-duck.jpg'),
+  new Image('dragon', 'img/dragon.jpg'),
+  new Image('pen', 'img/pen.jpg'),
+  new Image('pet-sweep', 'img/pet-sweep.jpg'),
+  new Image('scissors', 'img/scissors.jpg'),
+  new Image('shark', 'img/shark.jpg'),
+  new Image('sweep', 'img/sweep.png'),
+  new Image('tauntun', 'img/tauntaun.jpg'),
+  new Image('unicorn', 'img/unicorn.jpg'),
+  new Image('usb', 'img/usb.gif'),
+  new Image('water-can', 'img/water-can.jpg'),
+  new Image('wine-glass', 'img/wine-glass.jpg')
 ];
-console.log(images[0].idTag);
-//This is how to get to an images current url:   imageEls["0"].currentSrc
+
+var firstImageEl = document.getElementById('first-image');
+var secondImageEl = document.getElementById('second-image');
+var thirdImageEl = document.getElementById('third-image');
+console.log(firstImageEl);
+
+randomImages();
